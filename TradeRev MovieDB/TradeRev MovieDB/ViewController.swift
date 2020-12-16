540d604cf29fa4016dd1c8627edc86e511cfc56d
Serhii Pianykh <serhiipianykh@gmail.com>
2020-12-16 10:11:25 -0500

    Initial commit

---
+++ TradeRev MovieDB/TradeRev MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradeRev MovieDB
+//
+//  Created by Serhii Pianykh on 2020-12-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TradeRev MovieDB/TradeRev MovieDBTests/TradeRev_MovieDBTests.swift
@@ -0,0 +1,33 @@
+//
+//  TradeRev_MovieDBTests.swift
+//  TradeRev MovieDBTests
+//
+//  Created by Serhii Pianykh on 2020-12-16.
+//
+
+import XCTest
+@testable import TradeRev_MovieDB
+
+class TradeRev_MovieDBTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
